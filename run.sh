55b0e909f1d2e836e0614dc6db022ff372ed9d9c
rempartIrien <rempart.irien@gmail.com>
2025-01-22 13:43:20 +0100

    Initial commit

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Before running this script, connect with SSH just to get the SSH key fingerprint.
+# Otherwise, sshpass won't connect.
+#
+# Be sure to have all your domains set properly in your DNS provider manager.
+# Otherwise, Let's Encrypt will raise errors to Caddy.
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+ansible-playbook -i ./inventory.yml ./main.yml
